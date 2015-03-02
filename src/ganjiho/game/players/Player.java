fea2de7c0c8e235a099f942a6a985d472cfe94d3
Quang Tran <tran.quang@live.com>
2015-03-02 13:29:31 -0500

    Change parseRowChar to private and submit first deliverable.

--- src/ganjiho/game/players/Player.java
+++ src/ganjiho/game/players/Player.java
@@ -66,7 +66,7 @@ public abstract class Player
 		return move;
 	}
 	
-	public int parseRowChar(char row) throws Exception
+	private int parseRowChar(char row) throws Exception
 	{
 		switch(row)
 		{
