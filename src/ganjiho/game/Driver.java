d5f0f52b6c355116bd196226ecacc0b0cb41b5bd
Quang Tran <tran.quang@live.com>
2015-03-16 00:23:55 -0400

    Pull player instantiation into driver class.

--- src/ganjiho/game/Driver.java
+++ src/ganjiho/game/Driver.java
@@ -1,10 +1,22 @@
 package ganjiho.game;
 
+import ganjiho.game.players.BlackPlayer;
+import ganjiho.game.players.WhitePlayer;
+import ganjiho.heuristic.Heuristic;
+import ganjiho.heuristic.HeuristicRemainingMoves;
+
 public class Driver 
 {
 	public static void main(String[] args)
 	{
-		Board board = new Board(8);
+		WhitePlayer white = new WhitePlayer(null);
+		BlackPlayer black = new BlackPlayer(null);
+		
+		//Heuristic h = new HeuristicRemainingMoves(white);
+		//white = new WhitePlayer(new PlayerAI(h));
+		
+		Board board = new Board(8, white, black);
+		
 		try
 		{
 			while(board.isMoveAvailable())
